ccf27add6a73fd33734a34445fe94f11cb347bda
Eric Söderqvist <ericsoder@github.com>
2025-02-14 13:56:39 +0100

    Update

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import * as core from '@actions/core'
-import { wait } from './wait.js'
+import * as github from '@actions/github'
+const github_token: string = core.getInput('github_token')
+const octokit = github.getOctokit(github_token)
 
 /**
  * The main function for the action.
@@ -8,18 +10,86 @@ import { wait } from './wait.js'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const cost_center: string = core.getInput('cost_center')
+    const team_name: string = core.getInput('team_name')
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const costCenters = await octokit.request(
+      `GET /enterprises/${github.context.payload.enterprise?.name}/settings/billing/cost-centers`,
+      {
+        enterprise: github.context.payload.enterprise?.name,
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28'
+        }
+      }
+    )
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const costCenterUserList: string[] = []
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    costCenters.data.costCenters.forEach((costCenter: any) => {
+      costCenter.resources.forEach((resource: any) => {
+        if (resource.type === 'User') {
+          costCenterUserList.push(resource.name)
+          core.info(`User name: ${resource.name}`)
+        }
+      })
+    })
+
+    const teamMembers = await octokit.request(
+      `GET /orgs/${github.context.payload.repository?.owner}/teams/${team_name}/members`,
+      {
+        org: github.context.payload.repository?.owner,
+        team_slug: team_name
+      }
+    )
+
+    const teamMemberList: string[] = []
+
+    teamMembers.data.forEach((member: any) => {
+      if (member.login) {
+        teamMemberList.push(member.login)
+        core.info(`Team member name: ${member.title}`)
+      }
+    })
+
+    const usersNotInCostCenter = teamMemberList.filter(
+      (member: string) => !costCenterUserList.includes(member)
+    )
+
+    //add users to cost center
+    for (const user of usersNotInCostCenter) {
+      await octokit.request(
+        `PUT /enterprises/${github.context.payload.enterprise?.name}/settings/billing/cost-centers/${cost_center}/resources`,
+        {
+          enterprise: github.context.payload.enterprise?.name,
+          cost_center: cost_center,
+          resource: user,
+          resource_type: 'User',
+          headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+          }
+        }
+      )
+    }
+
+    const usersNotInTeam = costCenterUserList.filter(
+      (member: string) => !teamMemberList.includes(member)
+    )
+
+    //remove users from cost center
+    for (const user of usersNotInTeam) {
+      await octokit.request(
+        `DELETE /enterprises/${github.context.payload.enterprise?.name}/settings/billing/cost-centers/${cost_center}/resources`,
+        {
+          enterprise: github.context.payload.enterprise?.name,
+          cost_center: cost_center,
+          resource: user,
+          resource_type: 'User',
+          headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+          }
+        }
+      )
+    }
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
